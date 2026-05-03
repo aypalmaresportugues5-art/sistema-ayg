536536a55657e84d49701ad2c3e37b22d08d186c
aypalmaresportugues5-art <aypalmares.portugues5@gmail.com>
2026-05-02 23:16:14 -0400

    Update app.py

--- app.py
+++ app.py
@@ -149,9 +149,7 @@ elif menu == "Cuentas por Cobrar":
         # Leemos todas las ventas para calcular
         resp = requests.get(f"{URL_GOOGLE}?tipo=ventas")
         datos = resp.json()
-        df_v = pd.DataFrame(datos)
-                                                                                      
-        
+        df_v = pd.DataFrame(lista_ventas)      
         if not df_v.empty:
             # Filtramos por cliente
             df_cli = df_v[df_v['CLIENTE'] == cliente_sel]
