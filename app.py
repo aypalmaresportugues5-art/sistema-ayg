0c47c68710ec410dc8db950d1918d175ae5ae9e7
aypalmaresportugues5-art <aypalmares.portugues5@gmail.com>
2026-05-23 23:11:41 -0400

    Update app.py

--- app.py
+++ app.py
@@ -443,7 +443,7 @@ elif menu == "Cierre de Caja":
                 st.info("Aún no se han registrado ventas ni abonos en la jornada de hoy.")
         else:
             st.info("No se encontraron registros históricos de ventas.")
-elif opcion == "Simulador Costos":
+elif menu == "Simulador Costos":
         st.header("🥖 Simulador Unificado de Costos e Insumos")
         st.write("Calcula en tiempo real el costo bruto, operativo y sugerencia de PVP para tu producción.")
 
