5ab40c4e12fc17d840f8674173482a85d9befb6a
aypalmaresportugues5-art <aypalmares.portugues5@gmail.com>
2026-05-25 23:19:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -451,11 +451,12 @@ elif menu == "Simulador Costos":
         RECETAS_BASE = {
             "Pan Salado": {"HARINA": 45.0, "AGUA": 19.0, "AZUCAR": 3.0, "SAL": 1.0, "MANTECA": 2.0, "LEVADURA": 0.3, "ESENCIAS": 0.4, "peso_base": 0.05, "unidades_paquete": 12},
             "Pan de Perro": {"HARINA": 50.0, "AGUA": 19.0, "AZUCAR": 5.0, "SAL": 1.0, "MANTECA": 1.7, "LEVADURA": 0.25, "ESENCIAS": 0.4, "peso_base": 0.05, "unidades_paquete": 10},
-            "Polvorosas": {"HARINA": 4.5, "AZUCAR": 2.0, " AGUA":0.0, "MANTECA": 2.5, "peso_base": 0.04, "unidades_paquete": 1},
-            "Catalinas": {"HARINA": 5.0, "MIELINA": 3.5, "MELAO PAPELÓN": 2.0, "AGUA": 2.0, "ESENCIAS": 0.1, "ANIS Y CANELA": 2.0, "peso_base": 0.06, "unidades_paquete": 1},
+            "Polvorosas": {"HARINA": 4.5, "PH": 0.5, "AZUCAR": 2.0, " AGUA":0.0, "MANTECA": 2.5, "peso_base": 0.04, "unidades_paquete": 1},
+            "Catalinas": {"HARINA": 5.0, "SODA": 0.5, "MIELINA": 3.5, "MELAO PAPELÓN": 2.0, "AGUA": 2.0, "ESENCIAS": 0.1, "ANIS Y CANELA": 2.0, "peso_base": 0.06, "unidades_paquete": 1},
             "Receta Brownie": {"HARINA": 2.0, "AZUCAR": 3.0, "MANTECA": 1.5, "CACAO": 1.0, "HUEVOS": 1.2, "peso_base": 0.08, "unidades_paquete": 1},
             "PUDIN": {"RECORTES_PAN": 10.0, "LECHE": 5.0, "AZUCAR": 4.0, "HUEVOS": 1.5, "ESENCIAS": 0.2, "peso_base": 0.50, "unidades_paquete": 1},
             "Banquete (50 und)": {"HARINA": 5.0, "AGUA": 2.0, "AZUCAR": 0.5, "SAL": 0.1, "MANTECA": 0.4, "LEVADURA": 0.1, "peso_base": 0.03, "unidades_paquete": 50}
+            "PAN DE DULCE": {"HARINA": 1.0, "AGUA": 1.0, "AZUCAR": 1.0, "ANIS-CANELA": 1.0, "MANTECA": 1.0, "LEVADURA": 0.3, "ESENCIAS": 0.3, "GUAYABA": 0.4, "AREQUIPE": 0.4, "peso_base": 0.4, "unidades_paquete": 1}, 
         }
 
         # Selección de receta en pantalla
