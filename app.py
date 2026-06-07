f519d8f9fc5b687ecbfe58564230addcf61281fb
aypalmaresportugues5-art <aypalmares.portugues5@gmail.com>
2026-06-07 16:48:17 -0400

    Update app.py

--- app.py
+++ app.py
@@ -132,15 +132,15 @@ if st.session_state.pantalla == "Menu Principal":
    st.subheader("🎛️ SISTEMA AYG2017")
     
            # 🏪 Fila 1: Ventas
-col1, col2 = st.columns(2)
-with col1:
- if st.button("🏪\n\nVenta Detal", key="btn_detal", use_container_width=True):
-    st.session_state.pantalla = "Venta Detal"
-    st.rerun()
-with col2:
- if st.button("🛻\n\nVenta Mayor", key="btn_mayor", use_container_width=True):
-    st.session_state.pantalla = "Venta Mayor (SAYG)"
-    st.rerun()
+   col1, col2 = st.columns(2)
+   with col1:
+    if st.button("🏪\n\nVenta Detal", key="btn_detal", use_container_width=True):
+       st.session_state.pantalla = "Venta Detal"
+       st.rerun)
+   with col2:
+    if st.button("🛻\n\nVenta Mayor", key="btn_mayor", use_container_width=True):
+       st.session_state.pantalla = "Venta Mayor (SAYG)"
+       st.rerun()
 
 
 
