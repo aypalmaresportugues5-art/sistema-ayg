e8a3ddf4615282b9a1a1c0964fdf26495a9a5e39
aypalmaresportugues5-art <aypalmares.portugues5@gmail.com>
2026-06-07 23:49:22 -0400

    Update app.py

--- app.py
+++ app.py
@@ -197,13 +197,13 @@ if st.button("🚪 Cerrar Sesión / Salir", key="btn_salir", use_container_width
 # 📲 CONTROL DE LAS VENTANAS DE TRABAJO
 # =========================================================
 #else:
-if st.session_state.pantalla != "Menu Principal":
-   st.stop()
+ if st.session_state.pantalla != "Menu Principal":
+   #st.stop()
     # Botón de salida arriba en las pantallas secundarias
-if st.button("⬅️ Volver al Menú Principal", key="btn_volver"):
-   st.session_state.pantalla = "Menu Principal"
-   st.rerun()
-   st.markdown("---")
+ if st.button("⬅️ Volver al Menú Principal", key="btn_volver"):
+    st.session_state.pantalla = "Menu Principal"
+    st.rerun()
+    st.markdown("---")
 
     
 
