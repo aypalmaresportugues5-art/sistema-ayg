cd6af69f3e211c0183f53dcbf849726055da5bdf
aypalmaresportugues5-art <aypalmares.portugues5@gmail.com>
2026-06-09 09:34:53 -0400

    Update app.py

--- app.py
+++ app.py
@@ -829,25 +829,24 @@ with col6:
     formulario_cierre_de_caja(URL_GOOGLE)
 
 
-        # # Fila 4: Herramientas (Categoría en bloque Rojo/Rosa)
-        st.error("🛠️ HERRAMIENTAS ADICIONALES")
-        col7, col8 = st.columns(2)
-        
-        with col7:
-            if st.button("📊\n\nSimulador Costos", key="btn_simulador", use_container_width=True):
-                # 1. Intentamos descargar los costos actualizados
-                try:
-                    import pandas as pd
-                    enlace_excel = "https://docs.google.com/spreadsheets/d/1UczgRQ5ewH3M5ZfykdTz3DizPxgUnS2jtaY-dvXmg1I"
-                    url_publica = enlace_excel + "/export?format=csv&gid=1138925550"
-                    # Guardamos la tabla directamente en el casillero de la sesión
-                    st.session_state["df_costos_real"] = pd.read_csv(url_publica)
-                except Exception as e:
-                    # Si falla, guardamos una tabla vacía de respaldo en el mismo casillero
-                    st.session_state["df_costos_real"] = pd.DataFrame(columns=['Insumo', 'Costo Por Unidad'])
+ # # Fila 4: Herramientas (Categoría en bloque Rojo/Rosa)
+ st.error("🛠️ HERRAMIENTAS ADICIONALES")
+ col7, col8 = st.columns(2)
+ with col7:
+  if st.button("📊\n\nSimulador Costos", key="btn_simulador", use_container_width=True):
+     # 1. Intentamos descargar los costos actualizados
+     try:
+         import pandas as pd
+         enlace_excel = "https://docs.google.com/spreadsheets/d/1UczgRQ5ewH3M5ZfykdTz3DizPxgUnS2jtaY-dvXmg1I"
+         url_publica = enlace_excel + "/export?format=csv&gid=1138925550"
+         # Guardamos la tabla directamente en el casillero de la sesión
+         st.session_state["df_costos_real"] = pd.read_csv(url_publica)
+     except Exception as e:
+         # Si falla, guardamos una tabla vacía de respaldo en el mismo casillero
+         st.session_state["df_costos_real"] = pd.DataFrame(columns=['Insumo', 'Costo Por Unidad'])
                 
-                # 2. Llamamos a la función limpia (sin pasarle variables entre paréntesis)
-                formulario_simulador_costos()
+     # 2. Llamamos a la función limpia (sin pasarle variables entre paréntesis)
+     formulario_simulador_costos()
 
 
 
