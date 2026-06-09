2665d86ee89f95a628f859230dd7cece30dfc755
aypalmaresportugues5-art <aypalmares.portugues5@gmail.com>
2026-06-09 10:24:21 -0400

    Update app.py

--- app.py
+++ app.py
@@ -760,9 +760,10 @@ with col6:
   if st.button("📊\n\nSimulador Costos", key="btn_simulador", use_container_width=True):
       try:
           import pandas as pd
-          # Dirección completamente actualizada con tu ID nuevo y tu GID
+          # 🟢 Dirección corregida con la 'W' incluida:
           enlace_excel = "https://docs.google.com/spreadsheets/d/1UczgRQ5mvN3N5ZfykdTx3O4iPxgUVs2jtaV-dWXmgII"
           url_publica = enlace_excel + "/export?format=csv&gid=1138925550"
+
                     
           st.session_state["df_costos_real"] = pd.read_csv(url_publica)
       except Exception as e:
